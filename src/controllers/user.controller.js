76c99c5f5c2b091b3eba26ac9cf2729e03b14daf
Harsh Piplodiya <harshpiplodiya29@gmail.com>
2024-05-26 11:15:32 +0300

    added refresh token endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -107,8 +107,13 @@ const loginUser = asyncHandler(async (req, res) => {
     // send cookies
 
     const { email, username, password } = req.body;
+    // console.log(email);
 
-    if(!username || !email){
+    // if(!username && !email){
+    //     throw new ApiError(400, "Username or email is required!");
+    // }
+
+    if(!(username || email)){
         throw new ApiError(400, "Username or Email is required!");
     }
 
@@ -150,4 +155,73 @@ const loginUser = asyncHandler(async (req, res) => {
     )
 })
 
-export { registerUser, loginUser }
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1 // this removes the field from the documents
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out."))
+})
+
+// creating endpoint so that when the access token of the user expires after hitting the endpoint they'll be assigned a new access token and refresh token, if their refresh token and the one stores in the DB is the same i.e if the session expires of the user coz access tokens are short lived or for any other reason than the refresh token can be checked to log user back in / restart or continue the session of the user.
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "Unauthorized request.");
+    }
+
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+    
+        const user = await User.findById(decodedToken?._id);
+    
+        if(!user){
+            throw new ApiError(401, "Invalid Refresh Token.");
+        }
+    
+        if(incomingRefreshToken !== user.refreshToken){
+            throw new ApiError(401, "Refresh token is expired or used.");
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        const { accessToken, newRefreshToken } = await generateAccessAndRefreshToken(user._id);
+    
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                { accessToken, refreshToken: newRefreshToken },
+                "Access token refreshed."
+            )
+        )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token.");
+    }
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken }
