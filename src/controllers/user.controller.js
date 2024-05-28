52069472a5ef4a5ecf132b9d296640de5146b897
Harsh Piplodiya <harshpiplodiya29@gmail.com>
2024-05-28 10:09:38 +0300

    added getUserChannelProfile controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -329,6 +329,75 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "CoverImage updated successfully."))
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    if(!username?.trim()){
+        throw new ApiError(400, "Username is missing.");
+    }
+
+    const channel = await User.aggregate([
+        {   // 1st pipeline / stage -> in this we match the user
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {   // 2nd pipeline / stage -> counting the subscribers thorugh channel
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {   // 3rd pipeline / stage -> countign the channels subscribed_to through subscriber
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {   // 4th pipeline / stage -> adding some more fields to the OG User object / model.
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {   // 5th pipeline / stage -> 'project' is used for projection of only the fields that are demanded or necessary and not all.
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exist.")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User channel fetched successfully."))
+})
+
 export { 
     registerUser, 
     loginUser, 
@@ -338,5 +407,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
